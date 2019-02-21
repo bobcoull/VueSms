0f365ad018fbc6f1e239656219f57bb219de2a0f
bob <bobcoull@gmail.com>
2019-02-21 11:12:20 +0000

    Added api tests for Get and Post methods of SmsController

---
+++ tests/api.tests/Controllers/SmsController/Get_Should.cs
@@ -0,0 +1,101 @@
+﻿using api.Controllers;
+using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using twilioService.Interfaces;
+using Xunit;
+
+namespace api.tests.Controllers
+{
+    public class Get_Should
+    {
+        [Fact]
+        public void Get_ReturnsActionResult_With_200_Status_With_Valid_Parameters()
+        {
+            // assign
+            Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
+            twilioServiceMock
+                .Setup(x => x.GetMessages(It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(new GetMessagesResponse());
+
+            // act
+            var smsController = new SmsController(twilioServiceMock.Object);
+            var result = smsController.Get("20/02/2019 10:00:00", "22/02/2019 10:00:00");
+
+            // assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<GetMessagesResponse>(viewResult.Value);
+            Assert.True(model.IsSuccess);
+        }
+
+        [Fact]
+        public void Get_ReturnsActionResult_With_400_Status_With_Invalid_Valid_From_Date()
+        {
+            // assign
+            Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
+            var smsController = new SmsController(twilioServiceMock.Object);
+
+            // act
+            var result = smsController.Get("30/15/2019 10:00:00", "22/02/2019 10:00:00");
+
+            // assert
+            var viewResult = Assert.IsType<BadRequestObjectResult>(result);
+            var model = Assert.IsAssignableFrom<GetMessagesResponse>(viewResult.Value);
+            Assert.False(model.IsSuccess);
+            Assert.Equal(1, model.ErrorMessages.Count);
+        }
+
+        [Fact]
+        public void Get_ReturnsActionResult_With_400_Status_With_Invalid_Valid_To_Date()
+        {
+            // assign
+            Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
+            var smsController = new SmsController(twilioServiceMock.Object);
+
+            // act
+            var result = smsController.Get("22/02/2019 10:00:00", "30/15/2019 10:00:00");
+
+            // assert
+            var viewResult = Assert.IsType<BadRequestObjectResult>(result);
+            var model = Assert.IsAssignableFrom<GetMessagesResponse>(viewResult.Value);
+            Assert.False(model.IsSuccess);
+            Assert.Equal(1, model.ErrorMessages.Count);
+        }
+
+        [Fact]
+        public void Get_ReturnsActionResult_With_200_Status_With_Messges_For_Valid_Date_Range()
+        {
+            // assign
+            Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
+            DateTime fromDate = new DateTime(2019, 02, 10, 10, 0, 0);
+            DateTime toDate = new DateTime(2019, 02, 11, 10, 0, 0);
+            twilioServiceMock
+                .Setup(x => x.GetMessages(fromDate, toDate))
+                .Returns(new GetMessagesResponse
+                    {
+                        SmsMessages = new List<SmsMessage>
+                        {
+                            new SmsMessage {
+                                DateSent = new DateTime(2019, 02, 10, 10, 0, 0),
+                                MessageBody = "Tet Message"
+                            }
+                        }
+                    }
+                );
+
+            var smsController = new SmsController(twilioServiceMock.Object);
+
+            // act
+            var result = smsController.Get("10/02/2019 10:00:00", "11/02/2019 10:00:00");
+
+            // assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<GetMessagesResponse>(viewResult.Value);
+            Assert.True(model.IsSuccess);
+            Assert.Equal(1, model.SmsMessages.Count);
+        }
+    }
+}
--- tests/api.tests/Controllers/SmsController/Post_Should.cs
+++ tests/api.tests/Controllers/SmsController/Post_Should.cs
@@ -1,40 +1,25 @@
-﻿using System;
+﻿using api.Controllers;
+using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-using api.Controllers;
-using Microsoft.AspNetCore.Mvc;
-using System.Linq;
-using Moq;
 using twilioService.Interfaces;
-using BusinessObjects;
-using twilioService;
+using Xunit;
 
 namespace api.tests.Controllers
 {
-    public class SmsControllerTests
+    public class Post_Should
     {
-        [Fact]
-        public void Get_ReturnsActionResult_With200ttaus_With_Valid_Parameters()
-        {
-            // assign
-            Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
-
-            var smsController = new SmsController(twilioServiceMock.Object);
-            var result = smsController.Get("20/02/2019 10:00:00", "22/02/2019 10:00:00");
-
-            // Assert
-            var viewResult = Assert.IsType<OkObjectResult>(result);
-            var model = Assert.IsAssignableFrom<GetMessagesResponse>(viewResult.Value);
-            Assert.True(model.IsSuccess);
-        }
-
         [Fact]
         public void Post_ReturnsActionResult_With200Status_For_Valid_Message_Valid_Phone()
         {
             // assign
             Mock<ITwilioService> twilioServiceMock = new Mock<ITwilioService>();
+            twilioServiceMock
+                .Setup(x => x.SendMessage("hello world", "12345"))
+                .Returns(new SendMessageResponse());
 
             var smsController = new SmsController(twilioServiceMock.Object);
 
